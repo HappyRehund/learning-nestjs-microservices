76ffc09fd9c0c7a127b15cd531c4377888b67764
HappyRehund <rayhanardian045@gmail.com>
2026-02-08 15:38:03 +0700

    fix(catalog): fixing env not readed, so added config module for it

--- apps/catalog/src/catalog.module.ts
+++ apps/catalog/src/catalog.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CatalogController } from './catalog.controller';
 import { CatalogService } from './catalog.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [CatalogController],
   providers: [CatalogService],
 })
