73862eb9f2bbf8677b92b29ea684be5b3618da7b
HappyRehund <rayhanardian045@gmail.com>
2026-02-08 14:46:43 +0700

    feat(media): add entry point for media service with TCP transport

--- apps/media/src/main.ts
+++ apps/media/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { MediaModule } from './media.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MediaModule);
-  await app.listen(process.env.port ?? 3000);
+  process.title = 'catalog';
+  const logger = new Logger('CatalogBootstrap');
+  const PORT = Number(process.env.MEDIA_TCP_PORT ?? 4013)
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    MediaModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: PORT
+      }
+    }
+  )
+  app.enableShutdownHooks()
+  await app.listen();
+
+  logger.log(`Catalog microservices (TCP) listening on port ${PORT}`);
 }
 bootstrap();
