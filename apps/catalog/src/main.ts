a598857a9c86315d8f740bcc1ec6d061fd1979d8
HappyRehund <rayhanardian045@gmail.com>
2026-02-08 14:40:30 +0700

    feat(catalog): add entrypoint with TCP transport for catalog service

--- apps/catalog/src/main.ts
+++ apps/catalog/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { CatalogModule } from './catalog.module';
+import { Logger } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices'
 
 async function bootstrap() {
-  const app = await NestFactory.create(CatalogModule);
-  await app.listen(process.env.port ?? 3000);
+  process.title = 'catalog';
+  const logger = new Logger('CatalogBootstrap');
+  const PORT = Number(process.env.CATALOG_TCP_PORT ?? 4011)
+
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    CatalogModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: PORT
+      }
+    }
+  )
+  app.enableShutdownHooks();
+  await app.listen();
+
+  logger.log(`Catalog microservices (TCP) listening on port ${PORT}`)
 }
 bootstrap();
